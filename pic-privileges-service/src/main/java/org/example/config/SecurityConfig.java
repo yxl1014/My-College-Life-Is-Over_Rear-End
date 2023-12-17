80322a45807c4c1c92743071dc593fb3387276ae
sheen3 <sheen3@yeah.net>
2023-12-17 12:10:05 +0800

    #13[后端][用户权限][12.17]权限模块化完成扩展获取当前登陆用户信息（未完成）

--- pic-privileges-service/src/main/java/org/example/config/SecurityConfig.java
+++ pic-privileges-service/src/main/java/org/example/config/SecurityConfig.java
@@ -1,5 +1,6 @@
 package org.example.config;
 
+import org.example.service.impl.MyUserDetailsService;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -9,6 +10,7 @@ import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
